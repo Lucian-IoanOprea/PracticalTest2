00aad10d4af45541110c3432406f925d0b83e331
Lucian-IoanOprea <lucianone99@yahoo.com>
2022-05-30 08:36:19 +0300

    mesaj

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Constants.java
@@ -0,0 +1,11 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+public class Constants {
+    public static final String TAG = "[EIM Colocviu 2]";
+
+    final public static String WEB_SERVICE_ADDRESS = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+    final public static String GET = "get";
+    final public static String PUT = "put";
+}
+
